bd609e56b0b41b7e263409906656177b0d6570d5
himanshu7437 <himanshujangra368@gmail.com>
2025-05-01 11:48:08 +0530

    enhance response structure and ensure await for user updates in account management

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -295,7 +295,9 @@ const getCurrentUser = asyncHandler(async(req, res) => {
 
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(
+        new ApiResponse(200, req.user, "current user fetched successfully")
+    )
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -342,7 +344,7 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Error while uploading avatar")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -378,7 +380,7 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Error while uploading cover image")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await  User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
